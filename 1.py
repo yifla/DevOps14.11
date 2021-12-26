0ecdbc07f7fa5bebc95ec19a02e84f998d7dc98d
yifla <89942988+yifla@users.noreply.github.com>
2021-12-26 18:34:19 +0200

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,28 @@
+print("hello world")
+my_var = 10
+print(my_var)
+my_name = "yif"
+print(my_name)
+is_true = False
+my_list = ["yif", "lavy", 40, True, my_name]
+print(my_list[1])
+my_dict = {
+    "name": "yif",
+    "lname": "lavy",
+    "age" : 40,
+    "is_married" :True
+}
+print(my_dict)
+print(my_dict.keys())
+my_number = 5 * 2
+my_other = 5 * "yif"
+print(my_other)
+
+if my_number == 10:
+    print(my_number)
+
+fname = "yif"
+lname = "lavy"
+print ("hello" +" " + fname + " " + lname)
+print(f"hello {fname} {lname}")
+print("hello %s %s " % (lname, fname))
